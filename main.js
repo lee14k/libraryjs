4b7cfecf4ce7db34f971d1f556413c99b5a0490e
lee14k <113624108+lee14k@users.noreply.github.com>
2023-01-03 16:38:17 -0500

    added more logic to JS file

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-function addBook (title, author, pageNumber, read) {
-    this.title = title,
-    this.author  = author,
-    this.pageNumber = pageNumber,
-    this.read=read
+let addBook = document.getElementById('addbook')
+addBook.addEventListener('click', newBook)
+let library = []
+
+
+function newBook () {
+    let form = document.getElementById('newbook') 
+    if (form.style.display === 'none') {
+        form.style.display = 'block'
+    } else {
+        form.style.display='none'
+    }
 }
 
+
+class makeBooks {
+    constructor(title, author, pageNumber, dateRead) {
+        this.title = title,
+        this.author  = author,
+        this.pageNumber = pageNumber,
+        this.read=read
+    }
+}
