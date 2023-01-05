d4e62851087558be2f9c1dced71e14cfe33ab13b
lee14k <kmkovach@mtu.edu>
2023-01-05 09:42:43 -0500

    getting objects to display

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
-let addBook = document.getElementById('addbook')
-addBook.addEventListener('click', newBook)
-let library = []
+let addBook = document.getElementById("addbook");
+addBook.addEventListener("click", newBook);
+let library = [];
+let bookDisplay = document.getElementById("books");
 
+let makeBook = document.getElementById("submit");
+makeBook.addEventListener("click", addNewBook);
 
-function newBook () {
-    let form = document.getElementById('newbook') 
-    if (form.style.display === 'none') {
-        form.style.display = 'block'
-    } else {
-        form.style.display='none'
-    }
+function newBook() {
+  let form = document.getElementById("newbook");
+  if (form.style.display === "none") {
+    form.style.display = "block";
+  } else {
+    form.style.display = "none";
+  }
 }
 
+function addNewBook() {
+  let book = new Books(
+    (title = document.getElementById("title").value),
+    (author = document.getElementById("author").value),
+    (pageNumber = document.getElementById("numberpages").value),
+    (read = document.getElementById("dateread").value)
+  );
+  event.preventDefault();
+  library.push(book);
+  bookDisplay.innerText = library;
+}
 
-class makeBooks {
-    constructor(title, author, pageNumber, dateRead) {
-        this.title = title,
-        this.author  = author,
-        this.pageNumber = pageNumber,
-        this.read=read
-    }
+class Books {
+  constructor(title, author, pageNumber, read) {
+    (this.title = title),
+      (this.author = author),
+      (this.pageNumber = pageNumber),
+      (this.read = read);
+  }
 }
