6c7fcf095b76b0897a4f13f7429c6cd7f909c115
lee14k <kmkovach@mtu.edu>
2024-01-10 18:20:19 -0500

    revisiting for further understanding

--- main.js
+++ main.js
@@ -1,67 +1,40 @@
-let addBook = document.getElementById("addbook");
-addBook.addEventListener("click", newBook);
-let library = [];
-let bookDisplay = document.getElementById("books");
-let book 
-let libraryList
-let newt
-let craft = []
+const { constants } = require("fs/promises")
 
-let makeBook = document.getElementById("submit");
-makeBook.addEventListener("click", addNewBook);
+document.addEventListener('DOMContentLoaded', function() {
+  let library=[]
+  let addBook=document.getElementById('addBook')
+  let makeBook=document.getElementById('makeBook')
+  let form = document.getElementById('newbook')
+  class Book {
+    constructor(title, author, pages, read) {
+      this.title=title
+      this.author=author
+      this.pages=pages
+      this.read=read
+    }
+  }
+  addBook.addEventListener('click', function() {
+    form.style.display=form.style.display=="none" ? "block" : "none"
+  })
 
-
-const keyValue = (input) => Object.entries(input).forEach(([key,value]) => {
-  let booger = document.createElement('div');
-  booger.classList.add("book")
-  booger.innerText = (`${key} : ${value}`)
-document.querySelector("#books").appendChild(booger)
-  console.log(`${key} = ${value}`)
-  craft.push(`${key} = ${value}`)
+  makeBook.addEventListener('click', function(event) {
+    event.preventDefault()
+    const title=document.getElementById('title').value;
+    const author=document.getElementById('author').value;
+    const pageNumber = document.getElementById('numberpages').value;
+    const read=document.getElementById('read').checked;
+    const newBook=new Book(title, author, pageNumber, read)
+    addBookToLibrary(newBook);
+  })
+  function addBookToLibrary(book) {
+  keyValue(book)
+  library.push(book)
+  }
+  const keyValue=(book) => {
+    Object.entries(book).forEach(([key, value]) => {
+let bookElement=document.createElement('div')
+bookElement.classList.add('book')
+bookElement.innerText=`${key}:${value}` 
+document.querySelector("#books").appendChild(bookElement)
 })
-
-//allows form to display
-
-function newBook() {
-  let form = document.getElementById("newbook");
-  if (form.style.display === "none") {
-    form.style.display = "block";
-  } else {
-    form.style.display = "none";
-  }
-}
-
-
-
-function addNewBook() {
-  book = new Books(
-    (title = document.getElementById("title").value),
-    (author = document.getElementById("author").value),
-    (pageNumber = document.getElementById("numberpages").value),
-    (read = document.getElementById("dateread").value)
-
-  );
-  event.preventDefault();
-   // console.log(library)
-   keyValue(book)
-  //bookDisplay.innerText=craft
-   console.log(craft)
-
-
-   
-
-  }
-
-
-
-class Books {
-  constructor(title, author, pageNumber, read) {
-    (this.title = title),
-      (this.author = author),
-      (this.pageNumber = pageNumber),
-      (this.read = read);
-  }
-}
-
-
-
+  }})
