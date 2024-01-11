ea022003057fa590190dbe10246f02eb09da30b8
lee14k <kmkovach@mtu.edu>
2024-01-10 20:56:43 -0500

    working on basic adding books

--- main.js
+++ main.js
@@ -1,40 +1,46 @@
-const { constants } = require("fs/promises")
-
-document.addEventListener('DOMContentLoaded', function() {
-  let library=[]
-  let addBook=document.getElementById('addBook')
-  let makeBook=document.getElementById('makeBook')
-  let form = document.getElementById('newbook')
   class Book {
-    constructor(title, author, pages, read) {
-      this.title=title
-      this.author=author
-      this.pages=pages
-      this.read=read
-    }
+      constructor(title, author, pages, read) {
+          this.title = title;
+          this.author = author;
+          this.pages = pages;
+          this.read = read;
+      }
   }
-  addBook.addEventListener('click', function() {
-    form.style.display=form.style.display=="none" ? "block" : "none"
-  })
 
-  makeBook.addEventListener('click', function(event) {
-    event.preventDefault()
-    const title=document.getElementById('title').value;
-    const author=document.getElementById('author').value;
-    const pageNumber = document.getElementById('numberpages').value;
-    const read=document.getElementById('read').checked;
-    const newBook=new Book(title, author, pageNumber, read)
-    addBookToLibrary(newBook);
-  })
+  let library = [];
+  let addBook = document.getElementById('addbook');
+  console.log(addBook)
+  let form = document.getElementById('newbook');
+
+  addBook.addEventListener('click', function() {
+      form.style.display = form.style.display === "none" ? "block" : "none";
+  });
+
+  form.addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      const title = document.getElementById('title').value;
+      const author = document.getElementById('author').value;
+      const pageNumber = document.getElementById('numberpages').value;
+      const read = document.getElementById('read').value;
+
+      const newBook = new Book(title, author, pageNumber, read);
+      addBookToLibrary(newBook);
+  });
+
   function addBookToLibrary(book) {
-  keyValue(book)
-  library.push(book)
+      keyValue(book);
+      library.push(book);
+      console.log(book);
+      console.log(library);
   }
-  const keyValue=(book) => {
-    Object.entries(book).forEach(([key, value]) => {
-let bookElement=document.createElement('div')
-bookElement.classList.add('book')
-bookElement.innerText=`${key}:${value}` 
-document.querySelector("#books").appendChild(bookElement)
-})
-  }})
+
+  const keyValue = (book) => {
+      Object.entries(book).forEach(([key, value]) => {
+          let bookElement = document.createElement('div');
+          bookElement.classList.add('book');
+          bookElement.innerText = `${key}: ${value}`;
+          document.querySelector("#books").appendChild(bookElement);
+      });
+  }
+
